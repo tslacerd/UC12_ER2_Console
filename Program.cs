a6c1820300d56e70ca862c7b0d8e1f901c972547
tslacerd <tslacerd@gmail.com>
2022-03-26 22:23:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using CadastroPessoa.Classes;
+
+PessoaFisica novaPf = new PessoaFisica();
+
+novaPf.Nome = "novo nome";
+
+Console.WriteLine(novaPf.Nome);
+Console.WriteLine(novaPf.Nome);
+
+Console.WriteLine($"Nome: {novaPf.Nome} Rendimento: {novaPf.rendimento}");
+
